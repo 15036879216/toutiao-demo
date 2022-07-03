d107db3230997a9c69fa0b6516b3ba7e59fd1333
ZMW <2412969135@qq.com>
2022-07-03 11:13:59 +0800

    初始化了项目

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,14 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  data() {}
+}
+</script>
 
 <style lang="less">
 #app {
